4c5b031605a4e8de22990a005c4236e6059d18a8
mig-af <mkmouse786@gmail.com>
2025-06-20 20:21:44 +0000

    Cambiando tipo de dato del argumento int string en el index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ from api.controller import resumenController
 
 # Enlace de ruta al recurso
 
-api.add_resource(resumenController.ResumenController, "/api", "/api/<int:data>")
+api.add_resource(resumenController.ResumenController, "/api", "/api/<string:data>")
 
 app = app
 if __name__ == '__main__':
